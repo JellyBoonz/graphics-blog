7e84660fb7cc4e3452dd45da13138a9be349a3bb
Jaiden Ortiz <jaidenortiz@Jaidens-MacBook-Pro-2.local>
2025-10-20 07:16:33 -0400

    Update blog content - Mon Oct 20 07:16:33 EDT 2025

--- test-build.sh
+++ test-build.sh
@@ -6,16 +6,16 @@ echo "Testing Jekyll build..."
 # Try to build the site
 if command -v bundle &> /dev/null; then
     echo "Using Bundler..."
-    bundle exec jekyll build --baseurl "/graphics-blog"
+    bundle exec jekyll build
 else
     echo "Using direct Jekyll..."
-    jekyll build --baseurl "/graphics-blog"
+    jekyll build
 fi
 
 if [ $? -eq 0 ]; then
     echo "Build successful!"
     echo "Site built in _site directory"
-    echo "You can preview with: bundle exec jekyll serve --baseurl '/graphics-blog'"
+    echo "You can preview with: bundle exec jekyll serve"
 else
     echo "Build failed. Check the error messages above."
 fi
